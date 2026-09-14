e797a50b086a12efc17458af77b2a783f3287c3c
bexalinks-lab <bexalinks@gmail.com>
2026-09-14 21:12:58 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,10 @@
+// db.js
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/bexalink',
+});
+
+pool.on('error', (err) => console.error('[pg] unexpected error on idle client', err));
+
+module.exports = pool;
