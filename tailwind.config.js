e1fd7d1e0eb3b1cc8d06a099da8f2fbe72549c56
bexalinks-lab <bexalinks@gmail.com>
2026-09-17 12:22:55 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx}',
+    './frontend/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
